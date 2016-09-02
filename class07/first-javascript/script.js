ce723fc3c146787259efa4c3bfbafda921303242
shumeidenise <shumeidenise@gmail.com>
2016-09-01 21:47:04 -0400

    Class06 Startup Matchmaker Classwork Complete

---
+++ class07/first-javascript/script.js
@@ -0,0 +1,37 @@
+alert("Hi my name is Denise");
+
+document.write("this is some text");
+
+document.write('<br>');
+
+var num1= 10 * 10 ;
+
+document.write(num1);
+
+document.write('<br>');
+
+console.log(num1);
+
+document.write(100/10);
+
+document.write('<br>');
+
+document.write(50-17);
+
+document.write('<br>');
+
+document.write(50+17);
+
+document.write('<br>');
+
+document.write("Denise"+" "+"Shu Mei");
+
+document.write('<br>');
+
+// function
+function doSomething() {
+	console.log('this is working')
+}
+
+// call the function
+doSomething();
