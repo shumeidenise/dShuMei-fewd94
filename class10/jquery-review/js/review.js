2dae40b5493debd27873e6ebc7faea88242dfd1c
shumeidenise <shumeidenise@gmail.com>
2016-09-13 22:40:39 -0400

    Class10 update

---
+++ class10/jquery-review/js/review.js
@@ -0,0 +1,19 @@
+$('.box').css('background','#FF6347')
+$('div').text("this is here because of js")
+
+$('#first').click(function(){
+	$('#first').css('width', '400px')
+	$('#first').css('height', '400px')
+})
+
+$('#second').click(function(){
+	$('#second').css('width', '400px')
+	$('#second').css('height', '400px')
+})
+
+// $('#first',#'second').click(function(){
+// 	$(this).css('width', '400px')
+// 	$(this).css('height', '400px')
+// })
+
+//WHAT IS THE BONUS?!?!?!? 
