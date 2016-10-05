95d9c9e21b10ffa140eb296974bbadf56dda3862
shumeidenise <shumeidenise@gmail.com>
2016-10-04 20:43:31 -0400

    class17 googlemaps classwork complete
    including bike paths!

--- class-17/google-maps-example/js/app.js
+++ class-17/google-maps-example/js/app.js
@@ -1,4 +1,13 @@
+var map;
 
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+	  center: {lat: 40.7398476, lng: -73.9902068},
+	  zoom: 18
+	});
 
+var bikeLayer = new google.maps.BicyclingLayer();
+        bikeLayer.setMap(map);
+}
 
 
